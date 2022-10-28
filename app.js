01bc124571d12e3c71979fdce7287177e603e832
Shub1970 <18shubham1998@gmail.com>
2022-10-28 21:03:05 +0530

    add javascript

--- app.js
+++ app.js
@@ -11,6 +11,13 @@ app.post('/login', (req, res) => {
     const { firstName, lastName } = req.body;
     res.status(202).send(`welcome ${firstName} ${lastName}`)
 })
+app.post('/api/people', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({ success: false, msg: 'please provide name value' })
+    }
+    res.status(201).json({ success: true, person: name })
+})
 app.listen(3000, () => {
     console.log('server is listening in 3000---')
 })
