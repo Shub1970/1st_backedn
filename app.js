89a90481102741a47a6bf49990b10530017f8530
Shub1970 <18shubham1998@gmail.com>
2022-10-28 22:38:36 +0530

    javascript formate complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 app.use(express.static('./disp'));
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 const { people } = require('./data')
 app.get('/api/people', (req, res) => {
     res.status(202).send({ success: true, data: people })
@@ -13,9 +14,11 @@ app.post('/login', (req, res) => {
 })
 app.post('/api/people', (req, res) => {
     const { name } = req.body;
+    console.log(req.method);
     if (!name) {
         return res.status(400).json({ success: false, msg: 'please provide name value' })
     }
+
     res.status(201).json({ success: true, person: name })
 })
 app.listen(3000, () => {
--- disp/testjs.js
+++ disp/testjs.js
@@ -1,9 +1,8 @@
-const axios = require('axios').default;
 const result = document.querySelector('.result');
 const fetchPeople = async () => {
     try {
         const { data } = await axios.get('/api/people')
-        const people = data.map((person) => {
+        const people = data.data.map((person) => {
             return `<h5>${person.name}</h5>`
         })
         result.innerHTML = people.join('');
@@ -17,11 +16,11 @@ const btn = document.querySelector('.submit-btn');
 const input = document.querySelector('.form-input');
 const formAlert = document.querySelector('.form-alert');
 btn.addEventListener('click', async (e) => {
-    console.log("you click")
     e.preventDefault();
     const nameValue = input.value;
     try {
         const { data } = await axios.post('/api/people', { name: nameValue })
+        console.log(data);
         const h5 = document.createElement('h5');
         h5.textContent = data.person
         result.appendChild(h5);
